674190dba30cd08727ec176cacd61d83ab19f798
Victor Martins Dias <71146479+VictorMartinsDias@users.noreply.github.com>
2022-05-23 23:47:57 -0300

    Add files via upload

---
+++ ex001e.py
@@ -0,0 +1,5 @@
+print('Fala zeze, bom cara')
+
+
+
+
---
+++ ex003.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+print('A soma é {}'.format(n1 + n2))
+print('A soma entre', n1, 'e', n2, 'é', '{}'.format(n1 + n2))
+
+
+
+
+
